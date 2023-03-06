886df49cc6ff139f114836c3055ded77c262bdbf
duanjiatao <duanjiatao@xgd.com>
2023-03-06 09:37:44 +0800

    清空提交

--- com/djt/study/20230303/Test1.py
+++ com/djt/study/20230303/Test1.py
@@ -2,20 +2,20 @@
 class Score:
 
     def __int__(self):
-        self._score = 0
+        self.__score = 0
 
     def __set__(self, instance, value):
         if not isinstance(value, int):
             raise TypeError("Score must be integer")
         if not 0 <= value <= 100:
             raise ValueError("Valid value must be in [0, 100]")
-        self._score = value
+        self.__score = value
 
     def __get__(self, instance, owner):
-        return self._score
+        return self.__score
 
     def __delete__(self, instance):
-        del self._score
+        del self.__score
 
 
 # 普通类
@@ -29,11 +29,14 @@ class Student:
         self.math = math
         self.chinese = chinese
         self.english = english
+        self.__age = 18
 
     def __repr__(self):
         return "<Student: {}, math:{}, chinese: {}, english:{}>".format(
             self.name, self.math, self.chinese, self.english)
 
 
-xiao_ming = Student("小明", 76, 87, -10)
+xiao_ming = Student("小明", 76, 87, 80)
+print(xiao_ming)
+xiao_ming.name = "小王"
 print(xiao_ming)
